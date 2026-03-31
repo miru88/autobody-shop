8828a1814fb0fab2dc96a43a52ddb8d2f8686c8c
miru88 <38486864+miru88@users.noreply.github.com>
2026-03-31 00:17:35 -0400

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000',
+// };
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://autobody-shop-production.up.railway.app',
 };
